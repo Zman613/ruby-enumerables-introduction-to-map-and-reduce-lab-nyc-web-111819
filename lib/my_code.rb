f72c568d7843127382f697bb6d2793d9a2ceb91f
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 18:43:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,3 +32,15 @@ def map_to_square(source_array)
   end
   new_array
 end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
