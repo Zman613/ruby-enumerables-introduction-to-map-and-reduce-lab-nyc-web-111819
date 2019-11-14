7555303bf3f7859dd03a481c78ecf341b3f94336
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 18:57:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,14 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-
+  i = 0
+  while i < source_array.size do
+    if (source_array[i] == true)
+      return true
+    end
+    i += 1
+  end
+  return false
 end
 
 def reduce_to_any_true(source_array)
