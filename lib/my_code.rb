629069a85c1ed733f3ecea446db382d8b0c0de2f
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 18:46:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-
+  i = 0
+  new_array = []
+  while i < source_array.size do
+    new_array << source_array[i]
+    i += 1
+  end
+  new_array
 end
 
 def map_to_double(source_array)
